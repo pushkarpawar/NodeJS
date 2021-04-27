51f856bdc875ba93efe90abef467dd1d8d428bf3
unknown <iampushkarpawar@gmail.com>
2021-04-27 19:26:12 +0530

    Commiting

--- hello.js
+++ hello.js
@@ -3,4 +3,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Stay Home \n Stay Safe');
-}).listen(3000);
+}).listen(80);
